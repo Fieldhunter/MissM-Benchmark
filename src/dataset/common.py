3621c5f80c75ff6b4289f44215fbed2496bc5439
Fieldhunter <1677532160yuan@gmail.com>
2025-01-22 16:46:54 +0800

    add raw_data load

--- src/dataset/common.py
+++ src/dataset/common.py
@@ -1,4 +1,6 @@
 import torch
+import cv2
+import librosa
 
 from .basic import BasicDataset
 
@@ -22,3 +24,33 @@ class CocoClassification(BasicDataset):
     
     def __getitem__(self, idx):
         raise NotImplementedError
+
+
+def read_raw_text(file_path='text.txt'):
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    return content
+
+
+def read_raw_video(file_path='video.mp4', frame_interval=10):
+    cap = cv2.VideoCapture(file_path)
+
+    frame_list = []
+    frame_index = 0
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if frame_index % frame_interval == 0:
+            frame_list.append(frame)
+        frame_index += 1
+
+    return frame_list  # numpy list
+
+
+def read_raw_audio(file_path='audio.mp4'):
+    signal, sample_rate = librosa.load(file_path)
+
+    return signal, sample_rate  # signal shape: (n,)
